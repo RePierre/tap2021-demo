5f36b140948254dfd8af00831bf24ea96092ec46
RePierre <petru.rebeja@gmail.com>
2021-02-26 08:58:34 +0200

    Added console app to solution

---
+++ Tap2021Demo.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tap2021Demo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
