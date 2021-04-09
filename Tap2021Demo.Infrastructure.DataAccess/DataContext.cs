bd08883487094d6a1b6915226f3e24e7e7c8cbb3
RePierre <petru.rebeja@gmail.com>
2021-04-09 09:11:21 +0300

    Draft implementation of UoW and Repository

---
+++ Tap2021Demo.Infrastructure.DataAccess/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Tap2021Demo.Infrastructure.DataAccess
+{
+    public class DataContext : DbContext, IDataRepository, IUnitOfWork
+    {
+        public void Commit()
+        {
+            SaveChanges();
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            Delete(entity);
+        }
+
+        public TEntity Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            return Insert(entity);
+        }
+
+        public IQueryable<TEntity> Query<TEntity>() where TEntity : class
+        {
+            return Set<TEntity>();
+        }
+
+        void IDataRepository.Update<TEntity>(TEntity entity)
+        {
+            Update(entity);
+        }
+    }
+}
