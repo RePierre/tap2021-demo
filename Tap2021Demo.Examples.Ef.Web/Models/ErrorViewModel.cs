e3a50b6612300bbc38256aef8ba9a7b4dd2e376f
RePierre <petru.rebeja@gmail.com>
2021-04-09 08:41:02 +0300

    Added sample Web project for EF examples

---
+++ Tap2021Demo.Examples.Ef.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tap2021Demo.Examples.Ef.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
