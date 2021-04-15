74471965fa61aee233b64d88194ad3ab8634f148
RePierre <petru.rebeja@gmail.com>
2021-04-15 08:55:07 +0300

    Added project for entity mappings

---
+++ Tap2021Demo.DomainEntities.Mappings/EntityTypeConfigurationRegistrar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Tap2021Demo.Infrastructure.DataAccess;
+
+namespace Tap2021Demo.DomainEntities.Mappings
+{
+    public class EntityTypeConfigurationRegistrar : IEntityTypeConfigurationRegistrar
+    {
+        public void ApplyConfiguration(ModelBuilder modelBuilder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
