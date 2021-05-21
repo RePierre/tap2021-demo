8280f14386f8aa59559a871fe85edf1fc60dc9e5
RePierre <petru.rebeja@gmail.com>
2021-05-21 09:00:08 +0300

    Added extension method for setting up data access

---
+++ src/hackathon/Tap2021.Hotel.Infrastructure.DataAccess/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Tap2021.Hotel.Infrastructure.DataAccess
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection SetupDataAccessInfrastructure([NotNull] this IServiceCollection services, string connectionString)
+        {
+            return services.AddDbContext<DataContext>(opt => opt.UseSqlServer(connectionString))
+                .AddScoped<IDataRepository, DataContext>()
+                .AddScoped<IUnitOfWork, DataContext>();
+        }
+    }
+}
